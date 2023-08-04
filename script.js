b91dbe9a009840068e80b5f6031647b8959ae618
Omar Ashraf <114801560+omarafal@users.noreply.github.com>
2023-08-04 19:47:50 +0300

    Create occupied seats

--- script.js
+++ script.js
@@ -3,17 +3,24 @@ let numSeats = document.querySelector(".num-seats");
 let priceSeats = document.querySelector(".price-seats");
 
 seats.forEach(e => {
-    e.addEventListener("click", () => {
-        // check if the button has already been selected
-        if(e.classList[1] == "selected"){
-            e.classList.remove("selected");
-            numSeats.innerHTML = parseInt(numSeats.innerHTML)-1;
-            priceSeats.innerHTML = parseInt(priceSeats.innerHTML)-20;
-        }
-        else{
-            e.classList.add("selected");
-            numSeats.innerHTML = parseInt(numSeats.innerHTML)+1;
-            priceSeats.innerHTML = parseInt(priceSeats.innerHTML)+20;
-        }
-    })
+    // check if the seat has been occupied
+    if(e.classList[1] == "occupied"){
+        e.innerHTML = "X";
+    }
+    else{
+        e.addEventListener("click", () => {
+            // check if the button has already been selected
+            if(e.classList[1] == "selected"){
+                e.classList.remove("selected");
+                numSeats.innerHTML = parseInt(numSeats.innerHTML)-1;
+                priceSeats.innerHTML = parseInt(priceSeats.innerHTML)-20;
+            }
+            else{
+                e.classList.add("selected");
+                numSeats.innerHTML = parseInt(numSeats.innerHTML)+1;
+                priceSeats.innerHTML = parseInt(priceSeats.innerHTML)+20;
+            }
+        })
+    }
+  
 })
